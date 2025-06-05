c606508951ee3ffc06a5c129217e709411e8fde9
[Malik Hubbard] <[malik.hubbard@splight.com]>
2025-06-05 14:18:24 -0700

    Updated README and ran script on SplightSim

--- scripts/segment_updater.py
+++ scripts/segment_updater.py
@@ -12,33 +12,26 @@ from splight_lib.models import Asset
 def main(): 
     altitude_client = utils.OpenElevationClient()
     all_assets = Asset.list(type__in="Segment")
-    i = 0
 
     asset_dict: dict[str, utils.Tower] = {}
     for asset in tqdm(all_assets, desc="Creating segment dictionary", unit="segments"):
-        if i < 2: 
-            full_asset = Asset.retrieve(asset.id)
-            asset_dict[asset.name] = utils.Tower(full_asset, altitude_client)
-            i += 1
+        full_asset = Asset.retrieve(asset.id)
+        asset_dict[asset.name] = utils.Tower(full_asset, altitude_client)
 
     #go through each segment and update the database
-    j = 0 #guard so that only one segment is updated during testing
-    for asset_name in asset_dict.keys():
-        if j == 0:
-            print(f"Updating {asset_name}")
-            tower = asset_dict[asset_name]
-            utils.set_metadata(tower.altitude_id, str(tower.location.alt))
-            if tower.next_tower != None:
-                next_tower = asset_dict[tower.next_tower]
-                _ = utils.set_metadata(tower.distance_id, str(tower.location.distance_from(next_tower.location)))
-                span_length = tower.span_length_from(next_tower)
-                _ = utils.set_metadata(tower.span_length_id, str(span_length))
-                # utils.set_metadata(tower.span_length_id, "20")
-            else:
-                _ = utils.set_metadata(tower.distance_id, "0")
-                _ = utils.set_metadata(tower.span_length_id, "0")
-
-            j += 1
+    for asset_name in tqdm(asset_dict.keys(), desc="Updating metadata", unit="segments"):
+        # print(f"Updating {asset_name}")
+        tower = asset_dict[asset_name]
+        utils.set_metadata(tower.altitude_id, str(tower.location.alt))
+        if tower.next_tower in asset_dict.keys():
+            next_tower = asset_dict[tower.next_tower]
+            _ = utils.set_metadata(tower.distance_id, str(tower.location.distance_from(next_tower.location)))
+            span_length = tower.span_length_from(next_tower)
+            _ = utils.set_metadata(tower.span_length_id, str(span_length))
+            # utils.set_metadata(tower.span_length_id, "20")
+        else:
+            _ = utils.set_metadata(tower.distance_id, "0")
+            _ = utils.set_metadata(tower.span_length_id, "0")
 
 if __name__ == "__main__":
     main()   
