bc4d3937f735b0331934c306d0eab4147b827823
gbaudino-splight <geremias.baudino@splight-ae.com>
2025-06-29 14:36:56 -0300

    format

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,15 @@
-import pytest
 from pathlib import Path
+
+import pytest
+
 from scripts import utils
+
 
 @pytest.fixture
 def srtm_path():
     path = Path(__file__).resolve().parent.parent / "scripts/srtm"
-    return path 
+    return path
+
 
 @pytest.fixture
 def altitude_client():
