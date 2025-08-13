e2224cb067d88d12b019b879a057b211ee79a363
marianogaragiola <marianogaragiola@gmail.com>
2025-08-13 15:33:01 -0300

    feat: update script to run simulation and send values to hypersim

--- scripts/sim_traces.py
+++ scripts/sim_traces.py
@@ -1,28 +1,31 @@
 import os
 import sys
 import time
-import pandas as pd
+import json
+import argparse
 from datetime import datetime
 
-sys.path.append(r'C:\OPAL-RT\HYPERSIM\hypersim_2024.3.0.o30\Windows\HyApi\python')
-import HyWorksApiGRPC as HyWorksApi
+import pandas as pd
+
+# sys.path.append(r"C:\OPAL-RT\HYPERSIM\hypersim_2024.3.0.o30\Windows\HyApi\python")
+# import HyWorksApiGRPC as HyWorksApi
+
 
 def set_parameter(value, block, column, hour_str):
     try:
         value = float(value[column])
-        HyWorksApi.setComponentParameter(block, 'A', str(value))
+        # HyWorksApi.setComponentParameter(block, "A", str(value))
         print(f"[{hour_str}] Active: {column} → {block}.A = {value}")
     except Exception as e:
         print(f"[{hour_str}] Error active {column} → {block}: {e}")
 
 
-
 def process(df_active, df_reactive, active_mapping, reactive_mapping):
     now = datetime.utcnow().replace(second=0, microsecond=0)
-    hora_str = now.strftime('%H:%M:%S')
+    hora_str = now.strftime("%H:%M:%S")
 
-    active_row = df_active[df_active['hour'] == hora_str]
-    reactive_row = df_reactive[df_reactive['hour'] == hora_str]
+    active_row = df_active[df_active["hour"] == hora_str]
+    reactive_row = df_reactive[df_reactive["hour"] == hora_str]
 
     if not active_row.empty and not reactive_row.empty:
         valores_activa = active_row.iloc[0]
@@ -37,52 +40,131 @@ def process(df_active, df_reactive, active_mapping, reactive_mapping):
         print(f"[{hora_str}] Data not found for this hour.")
 
 
+class HypersimSimulator:
+    def __init__(self, design_path: str):
+        self.design_path: str = design_path
+        self.devices: dict = {}
+        self.metrics_ref: dict[str, pd.DataFrame] = {}
+
+    def add_metric_reference(self, metric: str, df: pd.DataFrame) -> None:
+        self.metrics_ref.update({metric: df})
+
+    def add_device(
+        self, device_name: str, device_metric: dict[str, str]
+    ) -> None:
+        """Add a device to the simulation.
+
+        Parameters
+        ----------
+        device_name : str
+            Name of the device to be added.
+        device_metric : str
+            Metric of the device to be added.
+        """
+        if device_name in self.devices:
+            raise ValueError(f"Device {device_name} already exists.")
+        if device_name not in self.metrics_ref:
+            raise ValueError(
+                f"Device {device_name} has no metrics table associated."
+            )
+        self.devices.update({device_name: device_metric})
+
+    def start(self) -> None:
+        if not self.metrics_ref:
+            raise ValueError("No metrics tables added to the simulation.")
+        self._start_simulation()
+
+        self._run_simulation_loop()
+        __import__("ipdb").set_trace()
+
+    def stop(self) -> None:
+        print("Stopping simulation ...")
+        # HyWorksApi.stopSim()
+        print("Simulation stopped ...")
+        __import__("ipdb").set_trace()
+
+    def _run_simulation_loop(self) -> None:
+        # TODO: Implement while True to send data after each minute
+
+    def _start_simulation(self) -> None:
+        print("Starting simulation ...")
+        # HyWorksApi.startAndConnectHypersim()
+        # HyWorksApi.openDesign(self.design_path)
+        # HyWorksApi.startSim()
+        print("Simulation started ...")
+
 
 def main():
+    # argparse loader: single positional JSON config file
+    parser = argparse.ArgumentParser(
+        description="Run Hypersim simulation with config"
+    )
+    parser.add_argument(
+        "--config-file",
+        "-c",
+        help="Path to JSON configuration file",
+    )
+    args = parser.parse_args()
 
-    HyWorksApi.startAndConnectHypersim()
-    design_path = "C:\\Users\\lucas\\OneDrive\\Documentos\\HYPERSIM\\Black Box\\Python\\CambioDePgen1.ecf"
-    HyWorksApi.openDesign(design_path)
-    HyWorksApi.startSim()
-    print("Simulation started...")
+    with open(args.config_file, "r", encoding="utf-8") as config_file:
+        config = json.load(config_file)
 
-    active_path = "C:\\Users\\lucas\\OneDrive\\Documentos\\HYPERSIM\\Black Box\\Python\\active_power.csv"
-    reactive_path = "C:\\Users\\lucas\\OneDrive\\Documentos\\HYPERSIM\\Black Box\\Python\\reactive_power.csv"
+    active_power_df = pd.read_csv(
+        config["input_files"]["active_power"], sep=",", encoding="utf-8"
+    )
+    reactive_power_df = pd.read_csv(
+        config["input_files"]["reactive_power"], sep=",", encoding="utf-8"
+    )
+    simulator = HypersimSimulator(
+        config["desing_path"],
+    )
+    simulator.add_metric_reference("active_power", active_power_df)
+    simulator.add_metric_reference("reactive_power", reactive_power_df)
+    for device_name, metrics in config["devices"]:
+        simulator.add_device(device_name, metrics)
 
-    df_active = pd.read_csv(active_path, sep=',', encoding='utf-8')
-    df_reactive = pd.read_csv(reactive_path, sep=',', encoding='utf-8')
-
-    for df in [df_active, df_reactive]:
-        df['timestamp'] = pd.to_datetime(df['timestamp'], format='%Y-%m-%d %H:%M:%S')
-        df['hour'] = df['timestamp'].dt.strftime('%H:%M:%S')
-
-    active_mapping = {
-        "P1": "PECalama",
-        "P2": "PEValleDeLosVientos",
-        "P3": "PFVAzabache",
-        "P4": "PFVJama",
-        "P5": "PFVSanPedro",
-        "P6": "PFVUsya"
-    }
-
-    reactive_mapping = {
-        "Q1": "PECalama",
-        "Q2": "PEValleDeLosVientos",
-        "Q3": "PFVAzabache",
-        "Q4": "PFVJama",
-        "Q5": "PFVSanPedro",
-        "Q6": "PFVUsya"
-    }
+    __import__("ipdb").set_trace()
 
     try:
-        while True:
-            process(df_active, df_reactive, active_mapping, reactive_mapping)
-            time.sleep(60)
+        simulator.start()
+    except Exception as exc:
+        print(f"Error starting simulation: {exc}")
+        simulator.stop()
+        sys.exit(1)
 
-    except KeyboardInterrupt:
-        print("\n Ctrl+C detected. Stopping simulation...")
-        HyWorksApi.stopSim()
-        print(" Simulation stopped and HYPERSIM closed successfully.")
+    # for df in [df_active, df_reactive]:
+    #     df["timestamp"] = pd.to_datetime(
+    #         df["timestamp"], format="%Y-%m-%d %H:%M:%S"
+    #     )
+    #     df["hour"] = df["timestamp"].dt.strftime("%H:%M:%S")
+
+    # active_mapping = {
+    #     "P1": "PECalama",
+    #     "P2": "PEValleDeLosVientos",
+    #     "P3": "PFVAzabache",
+    #     "P4": "PFVJama",
+    #     "P5": "PFVSanPedro",
+    #     "P6": "PFVUsya",
+    # }
+    #
+    # reactive_mapping = {
+    #     "Q1": "PECalama",
+    #     "Q2": "PEValleDeLosVientos",
+    #     "Q3": "PFVAzabache",
+    #     "Q4": "PFVJama",
+    #     "Q5": "PFVSanPedro",
+    #     "Q6": "PFVUsya",
+    # }
+
+    # try:
+    #     while True:
+    #         process(df_active, df_reactive, active_mapping, reactive_mapping)
+    #         time.sleep(60)
+    #
+    # except KeyboardInterrupt:
+    #     print("\n Ctrl+C detected. Stopping simulation...")
+    #     HyWorksApi.stopSim()
+    #     print(" Simulation stopped and HYPERSIM closed successfully.")
 
 
 if __name__ == "__main__":
